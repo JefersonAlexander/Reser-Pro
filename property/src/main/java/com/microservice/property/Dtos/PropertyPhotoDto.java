6b52262108ada09fdba64437ada251eeba9351a9
Carlos Andrey Henao Rincón <carlos.henao@f2x.com.co>
2025-10-27 13:14:23 -0500

    Adds Lombok annotations to PropertyPhotoDto.
    Adds Lombok annotations such as @Data, @NoArgsConstructor, @AllArgsConstructor, @Builder, and @JsonInclude to the PropertyPhotoDto class to reduce boilerplate code and improve data handling.

--- property/src/main/java/com/microservice/property/Dtos/PropertyPhotoDto.java
+++ property/src/main/java/com/microservice/property/Dtos/PropertyPhotoDto.java
@@ -1,5 +1,14 @@
 package com.microservice.property.Dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
 public class PropertyPhotoDto {
     private Long photoId;
     private String photoUrl;
