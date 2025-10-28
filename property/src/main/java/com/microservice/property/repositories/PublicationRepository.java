e854e74cd0bbd94592f7706af70b56d428ece460
Carlos Andrey Henao Rincón <carlos.henao@f2x.com.co>
2025-10-28 07:32:05 -0500

    Creates Publication repository
    Defines a repository interface for managing Publication entities.
    
    This interface extends JpaRepository to provide basic CRUD operations and
    includes a method to find publications by publication type.

---
+++ property/src/main/java/com/microservice/property/repositories/PublicationRepository.java
@@ -0,0 +1,12 @@
+package com.microservice.property.repositories;
+
+import com.microservice.property.entities.Publication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PublicationRepository extends JpaRepository<Publication, Long> {
+    Optional<Publication> findByPublicationType(String publicationType);
+}
